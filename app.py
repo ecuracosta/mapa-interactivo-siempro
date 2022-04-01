a1ab279b3d18a27c2adaad848bf2258dc5cc00ce
Emanuel Cura Costa <47532757+ecuracosta@users.noreply.github.com>
2022-04-01 18:05:44 -0300

    files needed

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_the_map():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
